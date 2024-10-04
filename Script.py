76e595979ad8388eb0795f1e13b8fe5dde762926
Yash <titanmattersbot05@gmail.com>
2024-10-04 19:44:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/super4crazy">super4crazy</a>」</b>"""
+    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/super4crazy">𝙎𝙪𝙥𝙚𝙧4𝘾𝙧𝙖𝙯𝙮</a>」</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
